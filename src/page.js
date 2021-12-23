dbd3cd83b00e5a183dba76221520c1e53145f979
dllddlld <91234352+dllddlld@users.noreply.github.com>
2021-12-23 12:05:08 +1100

    Change placeholder text

--- src/page.js
+++ src/page.js
@@ -2,41 +2,43 @@ const TEXT = {
     COMPANY_NAME: 'Gatsby\u2019s',
     COMPANY_SLOGAN: 'Let Us Serve You',
     COMPANY_MISSION: 'We strive to provide a space where guests can connect with themselves ' +
-    'and explore their full potential.',
-    CUSTOMER_BUBBLE: 'This is a text bubble',
-    WINNER_BUBBLE: 'This is another text bubble',
-    FOOD_SERVICE_POINT: 'Our servers practice social distancing to ensure you and your guests have a comfortable dining experience.',
+    'and enjoy local delicacies.',
+    CUSTOMER_BUBBLE: 'Voted best local venue of 2021',
+    WINNER_BUBBLE: '2 Michelin stars \u2605\u2605',
+    FOOD_SERVICE_POINT: 'Our servers practice social distancing to ensure you ' +
+        'and your guests have a comfortable dining experience.',
     VEGAN_POINT: 'We try our best to accommodate our diners\' dietary requirements.',
-    LOCAL_POINT: 'We believe is supporting local farmers and producers. We strive to source locally whenever possible.',
+    LOCAL_POINT: 'We believe is supporting local farmers and producers. ' + 
+        'We strive to source locally whenever possible.',
     MENU: {
         SURPRISE_ME: {
             TITLE: 'Surprise Me',
             PRICE: '$65 per head',
             DESCRIPTION: 'Our Surprise Me menu is a shared banquet-style feast ' + 
-            'that features a daily selection of the markets\' best and freshest, ' +
-            'selected by our executive chef. At least 3 dishes will be served for ' +
-            'each course and will include vegetable-based, seafood and meat options.'
+                'that features a daily selection of the markets\' best and freshest, ' +
+                'selected by our executive chef. At least 3 dishes will be served for ' +
+                'each course and will include vegetable-based, seafood and meat options.'
         },
         FEED_ME: {
             TITLE: 'Feed Me',
             PRICE: '$80 per head',
             DESCRIPTION: 'Our Feed Me menu starts with a shared entree, followed ' +
-            'by an a la carte main and finishing with a banquet-style dessert table ' +
-            'and cheese platters. Main options are a selection of vegetable-based, seafood and meat dishes.'
+                'by an a la carte main and finishing with a banquet-style dessert table ' +
+                'and cheese platters. Main options are a selection of vegetable-based, seafood and meat dishes.'
         },
         DINE_ME: {
             TITLE: 'Dine Me',
             PRICE: '$90 per head',
             DESCRIPTION: 'Our Dine Me menu is an all a la carte menu, with your choice of entree, main and dessert. ' +
-            'Entree and main options are a selection of vegetable-based, seafood and meat dishes.'
+                'Entree and main options are a selection of vegetable-based, seafood and meat dishes.'
         },
         INDULGE_ME: {
             TITLE: 'Indulge Me',
             PRICE: 'Min. $105 per head, final price may vary',
             DESCRIPTION: 'Our Indulge Me menu is a degustation-style menu designed in ' +
-            'consultation with our executive chef and our sommeliers. We will contact you ' +
-            'to design the best menu for the night. This is a dinner-only menu and ' +
-            'will be served throughout the course of 2-3 hours.'
+                'consultation with our executive chef and our sommeliers. We will contact you ' +
+                'to design the best menu for the night. This is a dinner-only menu and ' +
+                'will be served throughout the course of 2-3 hours.'
         }
     },
     CONTACT: {
